c47816198809f5abd495e2fa5f5a9a8b7ef4f060
hrithik16032000 <hrithikgavankar8582@gmail.com>
2020-08-01 23:38:14 +0530

    Updates

---
+++ precache-manifest.85697f793282d18171af4bfca644e340.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adbad63df1c0df91d691829432765eb8",
+    "url": "/React_Quiz_App/index.html"
+  },
+  {
+    "revision": "d1632c5a787c2c985641",
+    "url": "/React_Quiz_App/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "a522abb757f0a791f2e2",
+    "url": "/React_Quiz_App/static/js/2.003f0c85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Quiz_App/static/js/2.003f0c85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1632c5a787c2c985641",
+    "url": "/React_Quiz_App/static/js/main.aa76876a.chunk.js"
+  },
+  {
+    "revision": "d428a846e13b90851d35",
+    "url": "/React_Quiz_App/static/js/runtime-main.06b26501.js"
+  }
+]);
