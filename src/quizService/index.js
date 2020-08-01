52de613c5edad4e247ed3e40a38d7599876859be
hrithik16032000 <hrithikgavankar8582@gmail.com>
2020-08-01 23:27:50 +0530

    React Quiz App

---
+++ src/quizService/index.js
@@ -0,0 +1,375 @@
+const qBank = [
+  {
+    question:
+      "Virgin Trains, Virgin Atlantic and Virgin Racing, are all companies owned by which famous entrepreneur?   ",
+    answers: ["Alan Sugar","Richard Branson","Donald Trump", "Bill Gates"],
+    correct: "Richard Branson",
+    questionId: "099099"
+  },
+  {
+    question:
+      'Where is the train station "Mumbai Central"?',
+    answers: ["Wales", "Moldova", "India", "Denmark"],
+    correct: "India",
+    questionId: "183452"
+  },
+  {
+    question:
+      "Who is the owner of Amazon?",
+    answers: ["Mark Zuckerberg", "Jeff Bezos", "Bill Gates", "Elon Musk"],
+    correct: "Jeff Bezos",
+    questionId: "267908"
+  },
+  {
+    question: "What's the name of Batman's  parents?",
+    answers: [
+      "Thomas & Martha",
+      "Joey & Jackie",
+      "Jason & Sarah",
+      "Todd & Mira"
+    ],
+    correct: "Thomas & Martha",
+    questionId: "333247"
+  },
+  {
+    question: "What is the most common form of democracy?",
+    answers: ["Consensus", "Representative", "Deliberative", "Legitmate"],
+    correct: "Representative",
+    questionId: "496293"
+  },
+  {
+    question:
+      "What was the name of the WWF professional wrestling tag team made up of the wrestlers Ax and Smash?",
+    answers: [
+      "The Dream Team",
+      "The Bushwhackers",
+      "The British Bulldogs",
+      "Demolition"
+    ],
+    correct: "Demolition",
+    questionId: "588909"
+  },
+  {
+    question:
+      'What name represents the letter "M" in the NATO phonetic alphabet?',
+    answers: [ "Matthew", "Mike", "Mark", "Max"],
+    correct: "Mike",
+    questionId: "648452"
+  },
+  {
+    question: "What is the first book of the Old Testament?",
+    answers: ["Exodus", "Leviticus", "Genesis", "Numbers"],
+    correct: "Genesis",
+    questionId: "786649"
+  },
+  {
+    question:
+      "In the video-game franchise Kingdom Hearts, the main protagonist, carries a weapon with what shape?",
+    answers: ["Pen", "Sword", "Key", "Cellphone"],
+    correct: "Key",
+    questionId: "839754"
+  },
+  {
+    question:
+      "Which best selling toy of 1983 caused hysteria, resulting in riots breaking out in stores?",
+    answers: [
+      "Cabbage Patch Kids",
+      "Transformers",
+      "Care Bears",
+      "Rubik’s Cube"
+    ],
+    correct: "Cabbage Patch Kids",
+    questionId: "98390"
+  },
+  {
+    question: "What does a funambulist walk on?",
+    answers: ["Broken Glass", "Balls", "The Moon", "A Tight Rope"],
+    correct: "A Tight Rope",
+    questionId: "1071006"
+  },
+  {
+    question: "In past times, what would a gentleman keep in his fob pocket?",
+    answers: ["Money", "Watch", "Keys", "Notebook"],
+    correct: "Watch",
+    questionId: "1174154"
+  },
+  {
+    question: "Pune is located in which Indian state?",
+    answers: ["Delhi", "Tamil Nadu", "Gujarat", "Maharashtra"],
+    correct: "Maharashtra",
+    questionId: "1226535"
+  },
+  {
+    question: "How would one say goodbye in German?",
+    answers: ["Tchuss", " Hola", "Au Revoir", "Salir"],
+    correct: "Tchuss",
+    questionId: "1310938"
+  },
+  {
+    question: "What is the largest organ of the human body?",
+    answers: ["Heart", "large Intestine", "Skin", "Liver"],
+    correct: "Skin",
+    questionId: "1436873"
+  },
+  {
+    question: "Which sign of the zodiac is represented by the Crab?",
+    answers: ["Cancer", "Libra", "Virgo", "Sagittarius"],
+    correct: "Cancer",
+    questionId: "1515110"
+  },
+  {
+    question: "On a dartboard, what number is directly opposite No. 1?",
+    answers: ["20", "19", "12", "15"],
+    correct: "19",
+    questionId: "1642728"
+  },
+  {
+    question:
+      "What does the 'S' stand for in the abbreviation SIM, as in SIM card? ",
+    answers: ["Seven", "Subscriber", "Single", "Solid"],
+    correct: "Subscriber",
+    questionId: "1747256"
+  },
+  {
+    question:
+      "What word represents the letter 'T' in the NATO phonetic alphabet?",
+    answers: ["Tango", "Target", "Taxi", "Turkey"],
+    correct: "Tango",
+    questionId: "1822532"
+  },
+  {
+    question: "Which Leader appears on Indian Currency?",
+    answers: [
+      "Thomas Jefferson",
+      "Abraham Lincoln",
+      "Mahatma Gandhi",
+      "Benjamin Franklin"
+    ],
+    correct: "Mahatma Gandhi",
+    questionId: "195075"
+  },
+  {
+    question: 'What is "dabbing"?',
+    answers: ["A dance", "A medical procedure", "A sport", "A language"],
+    correct: "A dance",
+    questionId: "2019778"
+  },
+  {
+    question: "What is the name of the Jewish New Year?",
+    answers: ["Elul", "New Year", "Succoss", "Rosh Hashanah"],
+    correct: "Rosh Hashanah",
+    questionId: "2134343"
+  },
+  {
+    question: "Which one of the following rhythm games was made by Harmonix?",
+    answers: [
+      "Rock Band",
+      "Meat Beat Mania",
+      "Guitar Hero Live",
+      "Dance Dance Revolution"
+    ],
+    correct: "Rock Band",
+    questionId: "2210799"
+  },
+  {
+    question:
+      "Which animal is most endangered in India?",
+    answers: ["Leopard", "Tiger", "Panda", "Crocodile"],
+    correct: "Tiger",
+    questionId: "2379831"
+  },
+  {
+    question: "Red Vines is a brand of what type of candy?",
+    answers: ["Licorice", "Lollipop", "Chocolate", "Bubblegum"],
+    correct: "Licorice",
+    questionId: "2426418"
+  },
+  {
+    question: "What is the nickname of the US state of California?",
+    answers: ["Sunshine State", "Bay State", "Golden State",  "Treasure State"],
+    correct: "Golden State",
+    questionId: "2510086"
+  },
+  {
+    question: "Which year did australia got independence?",
+    answers: [
+      "1978",
+      "1966",
+      "1879",
+      "1901"
+    ],
+    correct: "1901",
+    questionId: "2685745"
+  },
+  {
+    question:
+      "What was the nickname given to the Hughes H-4 Hercules, a heavy transport flying boat which achieved flight in 1947?",
+    answers: ["Spruce Goose", "Noah's Ark", "Fat Man", "Trojan Horse"],
+    correct: "Spruce Goose",
+    questionId: "2796884"
+  },
+  {
+    question: "Which of these colours is NOT featured in the logo for Google?",
+    answers: ["Yellow", "Pink", "Blue", "Green"],
+    correct: "Pink",
+    questionId: "2838900"
+  },
+  {
+    question: 'What is the French word for "hat"?',
+    answers: ["Chapeau", "Bonnet", " Écharpe", " Casque"],
+    correct: "Chapeau",
+    questionId: "298060"
+  },
+  {
+    question: "Five dollars is worth how many rupees?",
+    answers: ["100", "50", "375", "69"],
+    correct: "375",
+    questionId: "3096579"
+  },
+  {
+    question: "Who is depicted on the US hundred dollar bill?",
+    answers: [
+      "Benjamin Franklin",
+      "George Washington",
+      "Abraham Lincoln",
+      "Thomas Jefferson"
+    ],
+    correct: "Benjamin Franklin",
+    questionId: "3182461"
+  },
+  {
+    question: "What do the letters in the GMT time zone stand for?",
+    answers: [
+      "Glasgow Man Time",
+      "Greenwich Mean Time",
+      "Global Meridian Time",
+      "General Median Time"
+    ],
+    correct: "Greenwich Mean Time",
+    questionId: "3239112"
+  },
+  {
+    question: "Which one of these is not a typical European sword design?",
+    answers: ["Scimitar", "Falchion", "Ulfberht", "Flamberge"],
+    correct: "Scimitar",
+    questionId: "3318503"
+  },
+  {
+    question:
+      'According to Sherlock Holmes, "If you eliminate the impossible, whatever remains, however improbable, must be the..."',
+    answers: ["Answer", "Cause", "Source", "Truth"],
+    correct: "Truth",
+    questionId: "3410327"
+  },
+  {
+    question: "What is the name of Poland in Polish?",
+    answers: ["Polska", "Pupcia", "Polszka", "Póland"],
+    correct: "Polska",
+    questionId: "3542596"
+  },
+  {
+    question: "The New York Times slogan is, “All the News That’s Fit to…”",
+    answers: ["Digest", "Print", "Look", "Read"],
+    correct: "Print",
+    questionId: "3667517"
+  },
+  {
+    question: "What do the letters of the fast food chain KFC stand for?",
+    answers: [
+      "Kentucky Fried Chicken",
+      "Kentucky Fresh Cheese",
+      "Kibbled Freaky Cow",
+      "Kiwi Food Cut"
+    ],
+    correct: "Kentucky Fried Chicken",
+    questionId: "3791672"
+  },
+  {
+    question: "Which restaurant's mascot is a clown?",
+    answers: ["McDonald's", "Whataburger", "Burger King", "Sonic"],
+    correct: "McDonald's",
+    questionId: "3893585"
+  },
+  {
+    question: 'What color is the "Ex" in FedEx Ground?',
+    answers: ["Red", "Light Blue", "Green", "Orange"],
+    correct: "Green",
+    questionId: "3913430"
+  },
+  {
+    question: "How tall is the Burj Khalifa?",
+    answers: ["2,722 ft", "2,717 ft", "2,546 ft", "3,024 ft"],
+    correct: "2,722 ft",
+    questionId: "4049121"
+  },
+  {
+    question:
+      "Which of the following card games revolves around numbers and basic math?",
+    answers: ["Go Fish", "Uno", "Twister", "Munchkin"],
+    correct: "Uno",
+    questionId: "4150746"
+  },
+  {
+    question:
+      "What machine element is located in the center of fidget spinners?",
+    answers: ["Bearings", "Axles", "Gears", "Belts"],
+    correct: "Bearings",
+    questionId: "4235063"
+  },
+  {
+    question: "Which sign of the zodiac comes between Virgo and Scorpio?",
+    answers: ["Gemini", "Taurus", "Capricorn", "Libra"],
+    correct: "Libra",
+    questionId: "4321002"
+  },
+  {
+    question: "Which of the following presidents is not on Mount Rushmore?",
+    answers: [
+      "John F. Kennedy",
+      "Theodore Roosevelt",
+      "Abraham Lincoln",
+      "Thomas Jefferson"
+    ],
+    correct: "John F. Kennedy",
+    questionId: "4442286"
+  },
+  {
+    question: "What is Tasmania?",
+    answers: [
+      "A flavor of Ben and Jerry's ice-cream",
+      "A Psychological Disorder",
+      "The Name of a Warner Brothers Cartoon Character",
+      "An Australian State"
+    ],
+    correct: "An Australian State",
+    questionId: "4564430"
+  },
+  {
+    question: "Which company developed android?",
+    answers: ["Microsoft", "Google", "Facebook", "Tesla"],
+    correct: "Google",
+    questionId: "4630606"
+  },
+  {
+    question:
+      "Which country, not including Japan, has the most people of japanese decent?",
+    answers: ["Brazil", "China", "South Korea", "United States of America"],
+    correct: "Brazil",
+    questionId: "4795960"
+  },
+  {
+    question: "Which candy is NOT made by Mars?",
+    answers: ["Cadbury", "Almond Joy", "M&M's","Snickers"],
+    correct: "Almond Joy",
+    questionId: "4811162"
+  },
+  {
+    question: "In which year Java was developed?",
+    answers: ["1999", "1997", "1990", "1995"],
+    correct: "1995",
+    questionId: "4982981"
+  }
+];
+
+export default (n = 5) =>
+  Promise.resolve(qBank.sort(() => 0.5 - Math.random()).slice(0, n));
